b4151989cf40173fe75664dd9a76774c2bb4ed73
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-23 15:09:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 
+/// This application can be played in the console and is based on the game Mastermind.
+/// The Console App will have the computer render a code that is 4 colors randomly selected
+/// from Red, Black, White, Green, Blue, or Yellow. The user has to try to guess the code and
+/// has 10 tries to crack the code. The computer will help the user understand how their guess
+/// lines up to the correct code by giving either White or Red Marks: Red Marks means that there
+/// is a correct color in the correct position and White Marks means that there is a correct color
+/// in the incorrect position.
+/// 
+/// </summary>
