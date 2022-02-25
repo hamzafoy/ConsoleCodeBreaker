1d36d31c682469c4d9fc8163ce1b902557e6d352
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-25 09:53:48 -0500

    Beginning to build an abstract 'Pegboard' class by which the Player and Opponent classes will be derived from and will alter the abstract PlacePegs() method.

---
+++ Pegboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCodeBreaker_Pegboard
+{
+    public abstract class Pegboard
+    {
+        public int[] Pegs = new int[] { 0, 0, 0, 0};
+
+        public abstract void PlacePegs();
+    }
+}
