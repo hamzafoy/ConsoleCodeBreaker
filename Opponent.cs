092e1156f2becfd04b8250a5c2477d1e7f0019ae
hamzafoy <hamzaoflouisville@gmail.com>
2022-03-02 10:28:02 -0500

    Adjusting StartUp() method's printed UI

--- Opponent.cs
+++ Opponent.cs
@@ -15,9 +15,9 @@ namespace ConsoleCodeBreaker_Opponent
             for(int i = 0; i < Pegs.Length; i++)
             {
                 Pegs[i] = pegGenerator.Next(5);
-                Console.Write("{0} ",Pegs[i]);
+                //Console.Write("{0} ",Pegs[i]);
             }
-            Console.WriteLine("\n\n");
+            //Console.WriteLine("\n\n");
         }
     }
 }
--- Player.cs
+++ Player.cs
@@ -11,14 +11,14 @@ namespace ConsoleCodeBreaker_Player
     {
         public override void PlacePegs()
         {
-            Console.WriteLine("You will need to 'place' your guesses\none at a time");
+            Console.WriteLine("\nYou will need to 'place' your guesses\none at a time");
             for(int i = 0; i < Pegs.Length; i++)
             {
                 string userInput = Console.ReadLine();
                 userInput = (!String.IsNullOrEmpty(userInput)) ? userInput : "0";
                 Int32.TryParse(userInput, out Pegs[i]);
             }
-            Console.WriteLine($"{Pegs[0]} {Pegs[1]} {Pegs[2]} {Pegs[3]}");
+            Console.WriteLine($"You submitted the following \nrow: {Pegs[0]} {Pegs[1]} {Pegs[2]} {Pegs[3]}\n");
         }
     }
 }
